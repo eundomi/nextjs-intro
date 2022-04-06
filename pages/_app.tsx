ef000983a470975bb7b76dff8fc46680258a80d7
SONEUNJUNG <sandy131712@gmail.com>
2022-04-06 18:29:57 +0900

    Custom App

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import { AppProps } from "next/dist/shared/lib/router/router";
+import NavBar from "../components/NavBar";
+import "../styles/globals.css";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <NavBar />
+      <Component {...pageProps} />
+      <style jsx global>{`
+        a {
+          color: white;
+        }
+      `}</style>
+    </>
+  );
+}
