82f7a5005185ceb259f3579a659f77c74862c5ec
SONEUNJUNG <sandy131712@gmail.com>
2022-04-07 18:15:26 +0900

    Server Side Rendering

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
 interface IMovie {
@@ -18,20 +17,12 @@ interface IMovie {
   vote_count: number;
 }
 
-export default function Home() {
-  const [movies, setMovies] = useState<IMovie[]>();
-  useEffect(() => {
-    ;(async () => {
-      const { results } = await (await fetch(`/api/movies`)).json();
-      setMovies(results);
-    })();
-  }, []);
-
+export default function Home({ results }: { results: IMovie[] }) {
   return (
     <div className="container">
       <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map((movie) => (
+      {!results && <h4>Loading...</h4>}
+      {results?.map((movie) => (
         <div className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
@@ -60,4 +51,11 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json();
+  return { props: { results } };
 }
