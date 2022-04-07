4c02d3a786913817a2571eff8bf203ccc657d529
SONEUNJUNG <sandy131712@gmail.com>
2022-04-07 14:11:56 +0900

    Fetching Data

---
+++ pages/index.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import Seo from "../components/Seo";
+
+const API_KEY = "856e16a8761c5a567b1791dee743f994";
+
+interface IMovie {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+export default function Home() {
+  const [movies, setMovies] = useState<IMovie[]>();
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
+
+  return (
+    <div>
+      <Seo title="Home" />
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
+    </div>
+  );
+}
